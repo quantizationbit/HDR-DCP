26cab45e659168ac68f43500aa074f08f5bef4d9
Bill Mandel <quantizationbit@gmail.com>
2018-03-15 04:27:01 -0700

    added 500 cd/m2

--- PQ300RGBP3_LUT.sh
+++ PQ300RGBP3_LUT.sh
@@ -9,6 +9,28 @@ popd
 
 CTL_MODULE_PATH="$EDRHOME/ACES/CTLa1:$EDRHOME/ACES/aces-dev/transforms/ctl/utilities:$EDRHOME/ACES/aces-dev/transforms/ctl/lib"
 
+# PQ500
+CUBE=65
+filename=lutimage.tiff
+lutname=PQ500P3
+ociolutimage --generate --cubesize $CUBE --output $filename
+
+ctlrender -force -verbose \
+    -ctl $EDRHOME/ACES/aces-dev/transforms/ctl/csc/ACEScct/ACEScsc.ACEScct_to_ACES.ctl -param1 aIn 1.0 \
+    -ctl $EDRHOME/ACES/aces-dev/transforms/ctl/rrt/RRT.ctl -param1 aIn 1.0 \
+    -ctl ./ODT.Academy.P3DCI_XXXXnits.ctl -param1 aIn 1.0 -param1 MAX 500.0 -param1 MIN 0.005 -param1 PQ 1 \
+$filename -format tiff16 $lutname".tiff"
+
+
+# Extract LUT
+rm -fv IDT_LMT.spi3d
+ociolutimage --extract --cubesize $CUBE --input $lutname".tiff" \
+  --output $lutname".spi3d"
+
+
+ociobakelut --lut $lutname".spi3d" --format iridas_itx --cubesize $CUBE $lutname".cube"
+
+
 
 # PQ300
 CUBE=65
